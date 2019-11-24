1b39e7cb285f6094ed319dd6294cc9c45f5f337a
ItayBS <Taytay884@yahoo.com>
2019-11-24 12:03:17 +0200

    Angular Material + ngRx + Angular 8- create the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {StoreModule} from '@ngrx/store';
+import {metaReducers, reducers} from './reducers';
+import {MatSliderModule} from "@angular/material";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
